1cc217633a79a01ec69b51bb92f9de3ee0c9c181
uyenphuong17 <161676500+uyenphuong17@users.noreply.github.com>
2024-06-19 08:37:32 +0700

    Delete DAMH(1) directory

--- DAMH(1)/src/main/java/com/example/DAMH/repository/CHITIETDATHANGRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.DAMH.repository;
-
-import com.example.DAMH.model.CHITIETDATHANG;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CHITIETDATHANGRepository extends JpaRepository <CHITIETDATHANG, Integer>{
-}

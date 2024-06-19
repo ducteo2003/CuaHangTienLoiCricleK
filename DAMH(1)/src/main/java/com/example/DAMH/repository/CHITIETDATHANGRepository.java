d88cfa212f2f52a15ecb0a4d18e34fd004eb8d4d
uyenphuong17 <161676500+uyenphuong17@users.noreply.github.com>
2024-06-19 08:41:52 +0700

    ko có mấy cái model key

---
+++ DAMH(1)/src/main/java/com/example/DAMH/repository/CHITIETDATHANGRepository.java
@@ -0,0 +1,7 @@
+package com.example.DAMH.repository;
+
+import com.example.DAMH.model.CHITIETDATHANG;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CHITIETDATHANGRepository extends JpaRepository <CHITIETDATHANG, Integer>{
+}
